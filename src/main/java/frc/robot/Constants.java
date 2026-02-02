21258ca78f7a0ebed3b4874852591ad40fdcb028
Abhinav Dashora <dashoraabhinav@gmail.com>
2026-02-02 11:24:46 -0600

    first commit

---
+++ src/main/java/frc/robot/Constants.java
@@ -0,0 +1,94 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot;
+
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.util.Units;
+
+/**
+ * The Constants class provides a convenient place for teams to hold robot-wide numerical or boolean
+ * constants. This class should not be used for any other purpose. All constants should be declared
+ * globally (i.e. public static). Do not put anything functional in this class.
+ *
+ * <p>It is advised to statically import this class (or one of its inner classes) wherever the
+ * constants are needed, to reduce verbosity.
+ */
+public final class Constants {
+
+
+  public static final double drive_kp = 1;
+  public static final double drive_ki = 0;
+  public static final double drive_kd = 0;
+
+  public static final double turn_kp = 1;
+  public static final double turn_ki = 0;
+  public static final double turn_kd = 0;
+
+
+  public static final double encoder_tick_ratio = 42;
+  public static final double drive_motor_ratio = 6.12;
+  public static final double turn_motor_ratio = 150/7;
+
+
+  public static final double position_conversion_factor = Units.feetToMeters(4*Math.PI /(encoder_tick_ratio * drive_motor_ratio));
+  public static final double velocity_conversion_factor = position_conversion_factor /60.0;
+  public static final double turn_position_conversion_factor = 360.0 / turn_motor_ratio;
+
+
+
+  public class dt{
+
+    public class module_0{
+
+      public static final int dirve_id = 1;
+      public static final int turn_id = 2;
+      public static final int cancoder_id = 3;
+      public static Rotation2d turn_offset = Rotation2d.fromDegrees(0);
+
+
+
+    }
+    public class module_1{
+
+      public static final int dirve_id = 4;
+      public static final int turn_id = 5;
+      public static final int cancoder_id = 6;
+      public static Rotation2d turn_offset = Rotation2d.fromDegrees(0);
+
+
+      
+    }
+    public class module_2{
+
+
+      public static final int dirve_id = 7;
+      public static final int turn_id = 8;
+      public static final int cancoder_id = 9;
+      public static Rotation2d turn_offset = Rotation2d.fromDegrees(0);
+
+      
+    }
+    public class module_3{
+
+      public static final int dirve_id = 10;
+      public static final int turn_id = 11;
+      public static final int cancoder_id = 12;
+      public static Rotation2d turn_offset = Rotation2d.fromDegrees(0);
+
+
+      
+    }
+
+
+
+
+  }
+
+
+
+  public static class OperatorConstants {
+    public static final int kDriverControllerPort = 0;
+  }
+}
---
+++ src/main/java/frc/robot/subsystems/Swerve_module.java
@@ -0,0 +1,104 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems;
+
+import com.ctre.phoenix.sensors.AbsoluteSensorRange;
+import com.ctre.phoenix.sensors.CANCoder;
+import com.revrobotics.RelativeEncoder;
+import com.revrobotics.spark.SparkMax;
+import com.revrobotics.spark.SparkBase.PersistMode;
+import com.revrobotics.spark.SparkBase.ResetMode;
+import com.revrobotics.spark.SparkLowLevel.MotorType;
+import com.revrobotics.spark.config.SparkMaxConfig;
+import com.revrobotics.spark.config.SparkBaseConfig.IdleMode;
+
+import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.math.geometry.Rotation2d;
+import frc.robot.Constants;
+
+/** Add your docs here. */
+public class Swerve_module {
+
+    public SparkMax drive_motor;
+    public SparkMax turn_motor;
+
+    public CANCoder best_encoder;
+    public RelativeEncoder drive_encoder;
+    public RelativeEncoder turn_encoder;
+
+    public int module_number;
+
+    PIDController turn_PID;
+    PIDController drive_PID;
+
+    Rotation2d turn_offset;
+    SparkMaxConfig drive_config;
+    SparkMaxConfig turn_config;
+
+    public AbsoluteSensorRange range;
+
+
+
+    public Swerve_module(int module_number, int dirve_id, int turn_id, int cancoder_id, Rotation2d turn_offset){
+
+
+
+        this.turn_offset = turn_offset;
+        this.drive_config = new SparkMaxConfig();
+        this.turn_config = new SparkMaxConfig();
+
+        this.module_number = module_number;
+
+        this.best_encoder = new CANCoder(cancoder_id);
+        range = AbsoluteSensorRange.Signed_PlusMinus180;
+        this.best_encoder.configAbsoluteSensorRange(range);
+
+        this.drive_motor = new SparkMax(dirve_id, MotorType.kBrushless);
+        this.drive_encoder = this.drive_motor.getEncoder();
+
+        drive_config
+            .voltageCompensation(12)
+            .smartCurrentLimit(50)
+            .idleMode(IdleMode.kBrake)
+            .inverted(false);
+        drive_config.encoder
+        .positionConversionFactor(Constants.position_conversion_factor)
+        .velocityConversionFactor(Constants.velocity_conversion_factor);
+
+        this.drive_motor.configure(drive_config,ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
+        
+
+        drive_PID = new PIDController(Constants.drive_ki, Constants.drive_kp, Constants.drive_kd);
+
+        this.turn_motor = new SparkMax(turn_id, MotorType.kBrushless);
+        this.turn_encoder = this.turn_motor.getEncoder();
+
+        turn_config
+            .voltageCompensation(12)
+            .smartCurrentLimit(20)
+            .idleMode(IdleMode.kBrake)
+            .inverted(false);
+
+            turn_config.encoder
+            .positionConversionFactor(Constants.turn_position_conversion_factor);
+            
+            turn_config.signals
+            .primaryEncoderPositionPeriodMs(500);
+
+
+
+            this.turn_motor.configure(turn_config, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
+
+            turn_PID = new PIDController(Constants.turn_kp, Constants.turn_ki, Constants.turn_kd);
+
+        
+
+
+
+
+    }
+    
+
+}
